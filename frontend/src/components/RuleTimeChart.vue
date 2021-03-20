7f83a90272e06ff2ebcdfd61a7a38d137842639c
Frank Jogeleit <fj@move-elevator.de>
2021-03-20 17:11:21 +0100

    Implement Model Table for Chart Events
    Signed-off-by: Frank Jogeleit <frank.jogeleit@web.de>

--- frontend/src/components/RuleTimeChart.vue
+++ frontend/src/components/RuleTimeChart.vue
@@ -8,6 +8,8 @@
         <apexchart type="bar" :height="height" :options="chart" :series="series"></apexchart>
       </wait>
     </v-card-text>
+
+    <event-overlay v-model="selectedEvents" />
   </v-card>
 </template>
 
@@ -17,6 +19,8 @@ import Vue, { PropType } from 'vue'
 import Wait from './Wait.vue'
 import { timeline, calculateStart } from '@/helper/time'
 import { HistoryCategories, Serie } from '@/types'
+import { ApexOptions } from 'apexcharts'
+import EventOverlay from './EventOverlay.vue'
 
 type Props = {
   time: number|null;
@@ -25,8 +29,6 @@ type Props = {
 
 type Computed = {
   chart: any;
-  series: Serie[];
-  categories: HistoryCategories;
 }
 
 type Data = {
@@ -35,10 +37,14 @@ type Data = {
   steps: number;
   height: number;
   range: number;
+  series: Serie[];
+  categories: HistoryCategories;
+  selectedEvents: FalcoEvent[];
+  eventsPerBucket: { [priority: string]: { [atep: number]: FalcoEvent[] } };
 }
 
 export default Vue.extend<Data, {}, Computed, Props>({
-  components: { Wait },
+  components: { Wait, EventOverlay },
   name: 'RuleTimeChart',
   props: {
     events: { type: Array as PropType<FalcoEvent[]>, required: true },
@@ -52,66 +58,80 @@ export default Vue.extend<Data, {}, Computed, Props>({
       steps,
       start,
       stepSize,
-      height: 400
+      height: 400,
+      series: [],
+      selectedEvents: [],
+      eventsPerBucket: {},
+      categories: []
     }
   },
   watch: {
-    events () {
-      this.start = calculateStart(this.range, this.stepSize)
+    events: {
+      immediate: true,
+      handler (events: FalcoEvent[]) {
+        const start = calculateStart(this.range, this.stepSize)
+
+        const categories = Array.from(Array(this.steps).keys()).reduce<HistoryCategories>((acc, step) => {
+          const time = start + ((step + 1) * this.stepSize)
+          let label = (new Date(time)).toISOString().replace('T', ' ').slice(0, -5)
+
+          if (this.range >= 60 * 60000) {
+            label = `${label.slice(0, -2)}00`
+          }
+
+          acc.push({ time, label })
+
+          return acc
+        }, [])
+
+        const eventsPerBucket: { [priority: string]: FalcoEvent[][] } = {}
+        const series: { [priority: string ]: Serie } = {}
+
+        events.forEach((event) => {
+          if (Object.prototype.hasOwnProperty.call(series, event.rule) === false) {
+            series[event.rule] = { name: event.rule, data: [...Array(this.steps).keys()].map(() => 0) }
+            eventsPerBucket[event.rule] = [...Array(this.steps).keys()].map(() => [])
+          }
+
+          if (event.filterTime < start) {
+            return
+          }
+
+          for (const index in categories) {
+            if (categories[(+index) + 1] && event.filterTime < categories[(+index) + 1].time) {
+              series[event.rule].data[index] += 1
+              eventsPerBucket[event.rule][index].push(event)
+              return
+            }
+          }
+
+          series[event.rule].data[this.steps - 1] += 1
+          eventsPerBucket[event.rule][this.steps - 1].push(event)
+        })
+
+        const sorted = Object.keys(series).sort().reduce<{ [rule: string]: Serie }>((acc, rule) => {
+          acc[rule] = series[rule]
+
+          return acc
+        }, {})
+
+        this.eventsPerBucket = eventsPerBucket
+        this.categories = categories
+        this.start = start
+        this.series = Object
+          .values(sorted)
+          .filter(serie => {
+            const sum = serie.data.reduce<number>((sum, count) => sum + count, 0)
+
+            return sum > 0
+          })
+      }
     }
   },
   computed: {
-    categories () {
-      return Array.from(Array(this.steps).keys()).reduce<Array<{ time: number; label: string }>>((acc, step) => {
-        const time = this.start + ((step + 1) * this.stepSize)
-        let label = (new Date(time)).toISOString().replace('T', ' ').slice(0, -5)
+    chart (): ApexOptions {
+      const labels = this.series.map(serie => serie.name)
 
-        if (this.range >= 60 * 60000) {
-          label = `${label.slice(0, -2)}00`
-        }
-
-        acc.push({ time, label })
-
-        return acc
-      }, [])
-    },
-    series () {
-      const unsorted: { [rule: string ]: Serie } = this.events.reduce<{ [rule: string ]: Serie }>((acc, event) => {
-        if (Object.prototype.hasOwnProperty.call(acc, event.rule) === false) {
-          acc[event.rule] = { name: event.rule, data: [...Array(this.steps).keys()].map(() => 0) }
-        }
-
-        if (event.filterTime < this.start) {
-          return acc
-        }
-
-        for (const index in this.categories) {
-          if (this.categories[(+index) + 1] && event.filterTime < this.categories[(+index) + 1].time) {
-            acc[event.rule].data[index] += 1
-            return acc
-          }
-        }
-
-        acc[event.rule].data[this.steps - 1] += 1
-
-        return acc
-      }, {})
-
-      const sorted = Object.keys(unsorted).sort().reduce<{ [rule: string]: Serie }>((acc, rule) => {
-        acc[rule] = unsorted[rule]
-
-        return acc
-      }, {})
-
-      return Object
-        .values(sorted)
-        .filter(serie => {
-          const sum = serie.data.reduce<number>((sum, count) => sum + count, 0)
-
-          return sum > 0
-        })
-    },
-    chart () {
       return {
         chart: {
           type: 'bar',
@@ -122,11 +142,26 @@ export default Vue.extend<Data, {}, Computed, Props>({
           },
           zoom: {
             enabled: false
+          },
+          events: {
+            dataPointSelection: (_: any, __: any, config: { dataPointIndex: number; seriesIndex: number }) => {
+              const rule = labels[config.seriesIndex]
+
+              this.selectedEvents = [...this.eventsPerBucket[rule][config.dataPointIndex]]
+            }
           }
         },
         plotOptions: {
           bar: {
             horizontal: false
+          }
+        },
+        states: {
+          active: {
+            allowMultipleDataPointsSelection: false,
+            filter: {
+              type: 'none'
+            }
           }
         },
         xaxis: {
