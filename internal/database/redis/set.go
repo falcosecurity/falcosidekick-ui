8ee5449279ad45426f716f922c96f1c34e6eb80f
Thomas Labarussias <issif+github@gadz.org>
2023-03-30 13:07:33 +0200

    allow dashes in hostnames and tag values
    Signed-off-by: Thomas Labarussias <issif+github@gadz.org>

--- internal/database/redis/set.go
+++ internal/database/redis/set.go
@@ -8,6 +8,7 @@ import (
 	"github.com/Issif/redisearch-go/redisearch"
 	"github.com/falcosecurity/falcosidekick-ui/configuration"
 	"github.com/falcosecurity/falcosidekick-ui/internal/models"
+	"github.com/falcosecurity/falcosidekick-ui/internal/utils"
 )
 
 func SetKey(client *redisearch.Client, event *models.Event) error {
@@ -21,12 +22,12 @@ func SetKey(client *redisearch.Client, event *models.Event) error {
 		Set("output", event.Output).
 		Set("source", event.Source).
 		Set("timestamp", event.Time.UnixNano()/1e3).
-		Set("tags", strings.Join(event.Tags, ",")).
+		Set("tags", utils.Escape(strings.Join(event.Tags, ","))).
 		Set("json", string(jsonString)).
 		Set("uuid", event.UUID).
 		SetTTL(c.TTL)
 	if event.Hostname != "" {
-		doc.Set("hostname", event.Hostname)
+		doc.Set("hostname", utils.Escape(event.Hostname))
 	}
 
 	err := client.Index([]redisearch.Document{doc}...)
